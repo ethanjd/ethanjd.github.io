63ae65b124947a3885e77879b43dcdba2711cb94
ethanjd <39056511+ethanjd@users.noreply.github.com>
2020-06-21 18:49:38 +0400

    Updated json url

--- Resources/picArray.js
+++ Resources/picArray.js
@@ -8,5 +8,5 @@ xhttp.onreadystatechange = function() {
 		fetchJSON(picArray);
 	}
 };
-xhttp.open("GET", "https://api.myjson.com/bins/dl9n6", true);
+xhttp.open("GET", "https://api.jsonbin.io/b/5eef735b2406353b2e09e2af", true);
 xhttp.send();
